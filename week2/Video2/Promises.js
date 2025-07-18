637a6bfd081c7ed21f941629130cbceabcf99a43
Ashish Kumar <ashishkumar141199@gmail.com>
2025-07-18 15:13:16 +0530

    change

--- week2/Video2/Promises.js
+++ week2/Video2/Promises.js
@@ -1,21 +1,21 @@
-// console.log("hello");
+console.log("hello");
 
-// class Rectangle {
-//   constructor(width, height, color) {
-//     this.width = width;
-//     this.height = height;
-//     this.color = color;
-//   }
+class Rectangle {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
 
-//   area() {
-//     const area = this.width * this.height;
-//     return area;
-//   }
+  area() {
+    const area = this.width * this.height;
+    return area;
+  }
 
-//   paint() {
-//     console.log(`Painting with color ${this.color}`);
-//   }
-// }
+  paint() {
+    console.log(`Painting with color ${this.color}`);
+  }
+}
 
 // const rect = new Rectangle(2, 4, "red");
 // const area = rect.area();
